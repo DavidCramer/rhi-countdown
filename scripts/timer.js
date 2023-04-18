57ee18ea76d222eb78e112372bae03a38a48b8c5
David Cramer <david@digilab.co.za>
2023-04-18 09:45:07 +0200

    colors

--- scripts/timer.js
+++ scripts/timer.js
@@ -6,6 +6,7 @@ const TimerApp = {
 	clockH: document.getElementById( 'hours' ),
 	clockM: document.getElementById( 'minutes' ),
 	clockS: document.getElementById( 'seconds' ),
+	tick: false,
 	init() {
 		const now = new Date();
 		setInterval( () => {
@@ -17,18 +18,24 @@ const TimerApp = {
 		const time1ms = time1.getTime( time1 );
 		const time2 = new Date( 2023, 3, 21, 15, 30 );
 		const time2ms = time2.getTime( time2 );
-
 		const difference = time2ms - time1ms;
-		var hours = Math.floor(difference / 36e5),
-			minutes = Math.floor(difference % 36e5 / 60000),
-			seconds = Math.floor(difference % 60000 / 1000);
+		var hours = Math.floor( difference / 36e5 ),
+			minutes = Math.floor( difference % 36e5 / 60000 ),
+			seconds = Math.floor( difference % 60000 / 1000 );
 		//const lapse = new Date( difference );
 		//const startHours = ( time2.getDay() - time1.getDay() ) * 24;
 		//this.clock.innerHTML = startHours;
-		this.clockH.innerHTML = `${hours}`;
-		this.clockM.innerHTML = `${minutes}`;
-		this.clockS.innerHTML = `${seconds}`;
+		this.clockH.innerHTML = `${ 10 > hours ? 0 : '' }${ hours }`;
+		this.clockM.innerHTML = `${ 10 > minutes ? 0 : '' }${ minutes }`;
+		this.clockS.innerHTML = `${ 10 > seconds ? 0 : '' }${ seconds }`;
 		//this.clock.innerHTML =  lapse.getMinutes() + ':' + lapse.getSeconds();
+		if ( false === this.tick ) {
+			this.clock.style.color = '#000000';
+			this.tick = true;
+		} else {
+			this.clock.style.color = '#c40505';
+			this.tick = false;
+		}
 	}
 
 };
