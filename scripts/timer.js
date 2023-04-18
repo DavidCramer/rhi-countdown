4caa7fd84ccf24f7da7cf64ef0d6ecc6fbe1959a
David Cramer <david@digilab.co.za>
2023-04-18 09:47:39 +0200

    texts

--- scripts/timer.js
+++ scripts/timer.js
@@ -19,23 +19,14 @@ const TimerApp = {
 		const time2 = new Date( 2023, 3, 21, 15, 30 );
 		const time2ms = time2.getTime( time2 );
 		const difference = time2ms - time1ms;
-		var hours = Math.floor( difference / 36e5 ),
-			minutes = Math.floor( difference % 36e5 / 60000 ),
-			seconds = Math.floor( difference % 60000 / 1000 );
-		//const lapse = new Date( difference );
-		//const startHours = ( time2.getDay() - time1.getDay() ) * 24;
-		//this.clock.innerHTML = startHours;
+		const hours = Math.floor( difference / 36e5 );
+		const minutes = Math.floor( difference % 36e5 / 60000 );
+		const seconds = Math.floor( difference % 60000 / 1000 );
+
 		this.clockH.innerHTML = `${ 10 > hours ? 0 : '' }${ hours }`;
 		this.clockM.innerHTML = `${ 10 > minutes ? 0 : '' }${ minutes }`;
 		this.clockS.innerHTML = `${ 10 > seconds ? 0 : '' }${ seconds }`;
-		//this.clock.innerHTML =  lapse.getMinutes() + ':' + lapse.getSeconds();
-		if ( false === this.tick ) {
-			this.clock.style.color = '#000000';
-			this.tick = true;
-		} else {
-			this.clock.style.color = '#c40505';
-			this.tick = false;
-		}
+
 	}
 
 };
