167a252117ee5e452d5e45cf6a6150b55117e7cd
David Cramer <david@digilab.co.za>
2023-04-24 07:44:16 +0200

    update time

--- scripts/timer.js
+++ scripts/timer.js
@@ -3,12 +3,14 @@
 const TimerApp = {
 	remaining: 0,
 	clock: document.getElementById( 'app' ),
+	date: document.getElementById( 'title' ),
+	clockD: document.getElementById( 'days' ),
 	clockH: document.getElementById( 'hours' ),
 	clockM: document.getElementById( 'minutes' ),
 	clockS: document.getElementById( 'seconds' ),
 	tick: false,
 	init() {
-		const now = new Date();
+		this.checkTimer();
 		setInterval( () => {
 			this.checkTimer();
 		}, 1000 );
@@ -19,10 +21,14 @@ const TimerApp = {
 		const time2 = new Date( 2023, 4, 5, 15, 30 );
 		const time2ms = time2.getTime( time2 );
 		const difference = time2ms - time1ms;
-		const hours = Math.floor( difference / 36e5 );
+		const days = Math.floor( difference / 36e5 / 24 );
+		const hours = Math.floor( difference / 36e5 - days * 24 );
 		const minutes = Math.floor( difference % 36e5 / 60000 );
 		const seconds = Math.floor( difference % 60000 / 1000 );
 
+
+		this.date.innerHTML = `${ time2.toString().replace(' GMT+0200 (South Africa Standard Time)', '') }`;
+		this.clockD.innerHTML = `${ 10 > days ? '0' : '' }${ days }`;
 		this.clockH.innerHTML = `${ 10 > hours ? '0' : '' }${ hours }`;
 		this.clockM.innerHTML = `${ 10 > minutes ? '0' : '' }${ minutes }`;
 		this.clockS.innerHTML = `${ 10 > seconds ? '0' : '' }${ seconds }`;
